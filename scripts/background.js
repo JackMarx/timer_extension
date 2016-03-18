beedb0c3fa0881b4ef55c531d677db4d54c5a7d6
Josh Nixon <seriousfools@gmail.com>
2016-03-17 19:39:04 -0500

    working on sending messages from background

--- scripts/background.js
+++ scripts/background.js
@@ -1,25 +1,45 @@
 (function () {
   "use strict";
 
-  chrome.tabs.onHighlighted.addListener(function(something){
-    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function(tabs){
-      $.ajax({
-        type: 'POST',
-        url: "http://andre-app.herokuapp.com/visits",
-        data:{url: tabs[0].url, time: Date.now(), user: "joe@gmail.com", action: "entered"},
-        success: function (data) {
-          alert("Success: " + data.url);
-        }
-      });
-    })
-  });
+  chrome.extension.onMessage.addListener(
+    function (request, sender, sendResponse) {
+      chrome.pageAction.show(sender.tab.id);
 
-  // chrome.tabs.onRemoved.addListener(function(something){
-  //   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-  //      function(tabs){
-  //         console.log({url: tabs[0].url, time: Date.now(), user: "joe@gmail.com", action: "removed"});
-  //      }
-  //   )
-  // });
+      if (request.command == 'saveToken') {
+          localStorage.setItem('timer_email', request.token);
+          sendResponse();
+          return true;
+      }
 
+      if (request.command == 'deleteToken') {
+          localStorage.setItem('timer_email', null);
+          sendResponse();
+          return true;
+      }
+    }
+  );
+
+  if (window.localStorage["timer_email"]){
+    chrome.tabs.onHighlighted.addListener(function(something){
+      chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function(tabs){
+        $.ajax({
+          type: 'POST',
+          url: "http://andre-app.herokuapp.com/visits",
+          data:{url: tabs[0].url, time: Date.now(), email: window.localStorage["timer_email"], action: "entered"},
+          success: function (response) {
+            // alert("success");
+          }
+        });
+      })
+    });
+
+    // chrome.tabs.onRemoved.addListener(function(something){
+    //   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    //      function(tabs){
+    //         console.log({url: tabs[0].url, time: Date.now(), user: "joe@gmail.com", action: "removed"});
+    //      }
+    //   )
+    // });
+
+  }
 }());
--- settings/js/settings.js
+++ settings/js/settings.js
@@ -3,22 +3,34 @@
 
   angular.module("app", []);
 
-  angular.module("app").controller("settingsCtrl", function($scope,$http){
-        var vault = window.localStorage;
-        
-        $scope.emailStored = false;
+  angular.module("app").controller("settingsCtrl", function($scope){
+    $scope.setup = function(){
+      $scope.vault = window.localStorage;
+      $scope.emailStored = false;
+    };
 
+    $scope.setToken = function(email_token) {
+      // sets or resets email token
+      null === email_token ? $scope.vault["timer_email"] = null : $scope.vault["timer_email"] = email_token;
+      if (localStorage["timer_email"] !== "undefined" || localStorage["timer_email"])
+        chrome.extension.sendMessage({
+          command: 'saveToken',
+          token: localStorage.getItem('timer_email')
+        }, function(data) {
+          $scope.emailStored = true;
+            // chrome.tabs.getCurrent(function (tab) {
+            //     chrome.tabs.remove(tab.id);
+            // });
+          });
+    } else {
+      chrome.extension.sendMessage({
+        command: 'deleteToken',
+        token: localStorage.getItem('timer_email')
+      }, function(data) {
+        $scope.emailStored = true;
+      });
+    // }
 
-        $scope.setToken = function(email_token) {
-            // sets or resets email token
-            null === email_token ? delete vault["timer_email"] : vault["timer_email"] = email_token}
-
-            if (vault["timer_email"]){
-              $scope.emailStored = true;
-            } else {
-              $scope.emailStored = false;
-            }
-          }
-
+  };
   });
 }());
